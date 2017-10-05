d6c674d6a187ea82e00c32dfd862327f6dad4983
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-10-06 00:48:58 +0300

    Unit test (#26)
    * Update constructor call
    
    * Remove demo constructor
    
    * Fix access modifier
    
    * Update unit test
    
    * Add ImageRecognition.DrawContours() test
    
    * Rename tests
    
    * Redo unit tests

--- social-tapTests/ImageRecognitionTests.cs
+++ social-tapTests/ImageRecognitionTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Collections;
 
 namespace social_tap.Tests
 {
@@ -12,10 +14,48 @@ namespace social_tap.Tests
     public class ImageRecognitionTests
     {
         [TestMethod()]
-        public void ImageRecognitionTest()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullArgument()
         {
-            ImageRecognition imageRecognition = new ImageRecognition();
-            Assert.IsNotNull(imageRecognition.GetProccessedImg());
+            new ImageRecognition(null);
+        }
+    }
+
+    [TestClass()]
+    public class ComparerTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Compare_NegativeNumArgument()
+        {
+            //Arrange
+            IComparer myComparer = new Comparer();
+
+            //Act
+            myComparer.Compare(7, 10);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Compare_NullArgument()
+        {
+            //Arrange
+            IComparer myComparer = new Comparer();
+
+            //Act
+            myComparer.Compare(null, null);
+        }
+    }
+
+
+    [TestClass()]
+    public class PixelCounterTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_NullArgument()
+        {
+            new PixelCounter(null);
         }
     }
 }
