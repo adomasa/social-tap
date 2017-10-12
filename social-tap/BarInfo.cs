ad7fbd260b63d704243a5b98f62f6e4e5ecd1b38
Adomas Aleknavičius <adomas.aleknavicius@mif.stud.vu.lt>
2017-10-12 21:40:48 +0300

    Rename BarInfo method to match method's naming requirements

---
+++ social-tap/BarInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace social_tap
+{
+    public class BarInfo
+    {
+        public int amount, sum;
+
+        public BarInfo(int n, int beverageLevel)
+        {
+            amount = n;
+            sum = beverageLevel;
+        }
+        public void Count(int n, int beverageLevel)
+        {
+            amount += 1;
+            sum += beverageLevel;
+        }
+    }
+}
