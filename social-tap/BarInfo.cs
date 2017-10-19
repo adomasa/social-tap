be19f7b3ec67fc5256e642897fc87f6f7618dae4
VALENTINAS <VALENTINAS@VALENTINAS-PC>
2017-10-19 20:14:03 +0300

    Add lazy

--- social-tap/BarInfo.cs
+++ social-tap/BarInfo.cs
@@ -9,16 +9,34 @@ namespace social_tap
     public class BarInfo
     {
         public int amount, sum;
+        /*public BarInfo(int n, int beverageLevel)
+        {
+            amount = n;
+            sum = beverageLevel;
+        }
+       
+        */
+
+        public Lazy<BarInfo> _barinfo;
+
+        public BarInfo(int sum)
+        {
+            this.sum = sum;
+        }
 
         public BarInfo(int n, int beverageLevel)
         {
             amount = n;
-            sum = beverageLevel;
+            _barinfo = new Lazy<BarInfo>(() =>
+            {
+                return new BarInfo(this.sum);
+            });
         }
         public void Count(int n, int beverageLevel)
         {
             amount += 1;
             sum += beverageLevel;
         }
+
     }
 }
