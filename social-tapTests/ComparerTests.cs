fb4315b8d3ea8e7ffa63df4b2b508c4961612798
Adomas Aleknavičius <adomas.aleknavicius@mif.stud.vu.lt>
2017-10-19 22:45:34 +0300

    Separate tests into different files

---
+++ social-tapTests/ComparerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace social_tapTests
+{
+    [TestClass()]
+    public class ComparerTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Compare_NegativeNumArgument()
+        {
+            //Arrange
+            IComparer myComparer = new social_tap.Comparer();
+
+            //Act
+            myComparer.Compare(7, 10);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Compare_NullArgument()
+        {
+            //Arrange
+            IComparer myComparer = new social_tap.Comparer();
+
+            //Act
+            myComparer.Compare(null, null);
+        }
+    }
+}
