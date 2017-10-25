5d433b40b05558338a078b06a28ae42860f2aa96
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-10-25 15:47:43 +0300

    Add references for the functional UI objects

--- Social-tap/MainActivity.cs
+++ Social-tap/MainActivity.cs
@@ -7,10 +7,17 @@ namespace Socialtap
     [Activity(Label = "Social-tap", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        EditText barNameEditText;
+        EditText commentEditText;
+        Button submitButton;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            barNameEditText = FindViewById<EditText>(Resource.Id.barNameEditText);
+            commentEditText = FindViewById<EditText>(Resource.Id.commentEditText);
+            submitButton = FindViewById<Button>(Resource.Id.submitButton);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
