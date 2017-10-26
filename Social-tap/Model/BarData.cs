5743697a8d5a161a25069f04fe1ae67fef7cb61c
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-10-26 19:46:14 +0300

    * IBarData.cs: Add bar data interface
    * BarReview.cs: Add bar review model class
    
    * BarData.cs: Add bar data model class implementing IBarData

---
+++ Social-tap/Model/BarData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Socialtap.Model
+{
+    public class BarData : IBarData
+    {
+        public String barName { get; private set; }
+        public int totalBeverageVolume { get; private set; }
+        public int avgBeverageVolume
+        {
+            get
+            {
+                return totalBeverageVolume / reviewCount;
+            }
+        }
+        public int avgRating
+        {
+            get
+            {
+                return totalRating / reviewCount;
+            }
+        }
+        public int totalRating { get; private set; }
+        public List<string> commentList { get; private set; }
+        public int reviewCount { get; private set; }
+
+        public BarData(string barName, int rating, string comment,
+                       int beverageVolume)
+        {
+            this.barName = barName;
+            this.totalRating = rating;
+            (commentList = new List<string>()).Add(comment);
+            this.totalBeverageVolume += beverageVolume;
+        }
+
+        public void AddReview(String comment, int rating, int beverageVolume)
+        {
+            commentList.Add(comment);
+            totalRating += rating;
+            totalBeverageVolume += beverageVolume;
+        }
+    }
+}
---
+++ Social-tap/Model/IBarData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Socialtap.Model
+{
+    public interface IBarData
+    {
+        String barName { get; }
+        int totalBeverageVolume { get; }
+        int totalRating { get; }
+        List<string> commentList { get; }
+        int reviewCount { get; }
+
+        void AddReview(String comment, int rating, int beverageVolume);
+    }
+}
