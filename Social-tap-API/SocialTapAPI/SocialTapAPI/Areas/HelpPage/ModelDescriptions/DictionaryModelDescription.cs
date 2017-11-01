3efd219a8fb749935c83dc597b5690311b9dc632
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-01 19:37:57 +0200

    Revert "Add first method to web service"
    This reverts commit ade25593336498b8728649bf667b05de382a537b.

--- Social-tap-API/SocialTapAPI/SocialTapAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SocialTapAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Social-tap-API/SocialTapAPI/SocialTapAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SocialTapAPI.WebApiApplication" Language="C#" %>
