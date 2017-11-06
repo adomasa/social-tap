521066f8876020be261c1f7cc77a6d7a30a448a2
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-06 15:51:59 +0200

    Transfer bar list fragment, update naming convetion

---
+++ Xamarin/Social-tap/Code/View_/Fragments/BarListFragment.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Socialtap.Code.Controller;
+
+namespace Socialtap.Code.View_.Fragments
+{
+    public class BarListFragment : Fragment
+    {
+        private RecyclerView _recyclerView;
+        private RecyclerView.LayoutManager _layoutManager;
+        private BarListAdapter _adapter;
+        private View _rootView;
+
+        public static BarListFragment NewInstance()
+        {
+            return new BarListFragment();
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            // Async kontrolerio užklausa listui gauti
+            MainActivityController.FetchBarsData();
+            
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            _rootView = inflater.Inflate(Resource.Layout.fragment_bar_list, container, false);
+            _recyclerView = _rootView.FindViewById<RecyclerView>(Resource.Id.list);
+            
+            _layoutManager = new LinearLayoutManager(Activity);
+            _recyclerView.SetLayoutManager(_layoutManager);
+                
+            _adapter = new BarListAdapter();
+
+            _recyclerView.SetAdapter(_adapter);
+            
+            return _rootView;
+        }
+    }
+}
