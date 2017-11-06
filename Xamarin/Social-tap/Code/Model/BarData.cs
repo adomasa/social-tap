3edd124fbdc7289b294d51316d1a987df6ae3a91
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-06 15:50:38 +0200

    Remove redudant BarData attributes and methods

--- Xamarin/Social-tap/Code/Model/BarData.cs
+++ Xamarin/Social-tap/Code/Model/BarData.cs
@@ -1,38 +1,27 @@
 ﻿using System.Collections.Generic;
 
-namespace Socialtap.Model
+namespace Socialtap.Code.Model
 {
     public class BarData : IBarData
     {
-        public string barName { get; }
-        public int totalBeverageVolume { get; private set; }
-        public int totalRating { get; private set; }
-        public List<string> commentList { get; }
-        public int reviewCount { get; private set; }
-
-        public int AvgBeverageVolume => totalBeverageVolume / reviewCount;
-        public int AvgRating => totalRating / reviewCount;
+        public string BarName { get; }
+        public int AverageBeverageVolume { get; }
+        public int AverageRating { get; }
+        public List<string> CommentList { get; }
+        public int ReviewCount { get; }
 
         public BarData(string barName, int rating, string comment,
-                       int beverageVolume)
+                       int beverageVolume, int reviewCount)
         {
-            this.barName = barName;
-            totalRating = rating;
-            (commentList = new List<string>()).Add(comment);
-            totalBeverageVolume += beverageVolume;
-            reviewCount = 1;
-        }
-
-        public void AddReview(string comment, int rating, int beverageVolume)
-        {
-            commentList.Add(comment);
-            totalRating += rating;
-            totalBeverageVolume += beverageVolume;
-            reviewCount++;
+            BarName = barName;
+            AverageRating = rating;
+            (CommentList = new List<string>()).Add(comment);
+            AverageBeverageVolume += beverageVolume;
+            ReviewCount = reviewCount;
         }
 
         public override string ToString() =>
-            $"[BarData: barName={barName}, totalBeverageVolume={totalBeverageVolume}, " +
-            $"totalRating={totalRating}, commentList={commentList}, reviewCount={reviewCount}]";
+            $"[BarData: barName={BarName}, totalBeverageVolume={AverageBeverageVolume}, " +
+            $"totalRating={AverageRating}, commentList={CommentList}, reviewCount={ReviewCount}]";
     }
 }
