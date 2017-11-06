a277938a45c41a59eef34236a692ee5daf20816a
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-06 15:51:22 +0200

    Add BarReview model class and interface

---
+++ Xamarin/Social-tap/Code/Model/BarReview.cs
@@ -0,0 +1,19 @@
+﻿namespace Socialtap.Code.Model
+{
+    public class BarReview : IBarReview
+    {
+        public string BarName { get; }
+        public int BeverageVolume { get; }
+        public int Rating { get; }
+        public string Comment { get; }
+        
+        public BarReview(int beverageVolume, int rating, string barName = "Nenurodyta",
+            string comment = "Komentaro nėra")
+        {
+            BarName = barName;
+            BeverageVolume = beverageVolume;
+            Rating = rating;
+            Comment = comment;
+        }
+    }
+}
---
+++ Xamarin/Social-tap/Code/Model/IBarReview.cs
@@ -0,0 +1,10 @@
+﻿namespace Socialtap.Code.Model
+{
+    public interface IBarReview
+    {
+        string BarName { get; }
+        int BeverageVolume { get; }
+        int Rating { get; }
+        string Comment { get; }
+    }
+}
