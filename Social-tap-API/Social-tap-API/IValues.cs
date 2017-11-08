903dfaac929304810e08af19acb1b2c780f46233
VALENTINAS <VALENTINAS@VALENTINAS-PC>
2017-11-08 14:39:48 +0200

    Change AddBarReview method return type

--- Social-tap-API/Social-tap-API/IValues.cs
+++ Social-tap-API/Social-tap-API/IValues.cs
@@ -7,7 +7,7 @@ namespace SocialtapAPI
 {
     interface IValues
     {
-        Dictionary<string, BarData> AdBarReview(string barName, string comment, int rate, int beverage);
+        Boolean AddBarReview(string barName, string comment, int rate, int beverage);
         Boolean Average(int beverageLevel);
         List<string> HashtagsFinder(string comment);
         Dictionary<string, List<string>> CountBars(string barName, string comment);
