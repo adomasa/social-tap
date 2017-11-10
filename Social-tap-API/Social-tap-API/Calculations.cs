929a0d1f71ee452b8add29275ac424491bd05635
VALENTINAS <VALENTINAS@VALENTINAS-PC>
2017-11-10 15:22:50 +0200

    Seperate web api methods and regular methods
    Add new unit tests
    Delete not used methods
    Add new class and interface

---
+++ Social-tap-API/Social-tap-API/Calculations.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SocialtapAPI
+{
+    public class Calculations:ICalculations
+    {
+        private static Dictionary<string, List<int>> _barRates = new Dictionary<string, List<int>>();
+        private List<string> _hashTags = new List<string>();
+        static int _uses;
+        static int _sum;
+        public const int MAX_BEVERAGE_LEVEL = 10; //kiek daugiausiai gali ipilti
+        public const int MAX_RATE = 5; // kiek daugiausiai gali duoti žvaigždučių 
+        public const int MIN_NAME_LENGHT = 1; //trumpiausias įmanomas baro pavadinimas
+        public const int MIN_BEVERAGE_RATE_LEVEL = 0; //kiek mažiausiai gali įpilti ir duoti žvaigždučių 
+        public Calculations(){
+
+        }
+
+        public string BarNameAdaptation(string barName)
+        {
+            barName = barName.ToLower();
+            barName = barName.Replace(" ", string.Empty).Replace("-", string.Empty).Replace(".", string.Empty);  // pasalinam visus tarpus, taškus ir -
+            barName = char.ToUpper(barName[0]) + barName.Substring(1);
+
+            return barName;
+        }
+        /// Į metodą paduodamas baro pavadinimas ir jo įvertinimas
+        /// Web service laikome baro pavadinimą ir jo įvertinimų Listą 
+        /// Atgal grąžiname tik baro įvertinimų vidurkį
+        /// http://localhost:.../api/values/barrate/string_baropavadinimas/string_įvertinimas
+        public double BarRateAverage(string barName, int rate)
+        {
+
+            if (_barRates.Keys.Contains(barName))
+            {
+                _barRates[barName].Add(rate);
+            }
+            else
+            {
+                _barRates[barName] = new List<int> { rate };
+            }
+            return _barRates[barName].Average();
+        }
+
+        /// Į metodą paduodama informacija kiek buvo įpilta 
+        /// Web service saugoma informacija apie tai kiek kartų buvo pasinaudota programa 
+        /// Grąžiname true jeigu įpilta geriau, false jeigu blogau. 
+        /// Lyginimas vyksta ne su vieno baro statistika o su BENDRA 
+        /// http://localhost:.../api/values/bevlvl/INT
+       // [HttpPost("bevlvl/{beverageLevel}")]    
+        public Boolean Average(int beverageLevel)
+        {
+            _uses++;
+            _sum += beverageLevel;
+            // sakysime, kad jeigu lygus vidurkiui, 
+            // tai ipilta geriau
+            return _sum / _uses <= beverageLevel;
+        }
+
+        /// Į metodą paduodamas komentaras 
+        /// Iš jo išrenkami žodžiai panaudoti su hashtagu 
+        /// Grąžinamas hashtagų listas 
+        /// http://localhost:.../api/values/tags/STRING
+        //  [HttpPost("tags/{comment}")]
+        public List<string> HashtagsFinder(string comment)
+        {
+            /*
+             * Hashtag'ą programoje reikės pakeist kuo nors kitu naudojant kintamasis.
+             * Replace("#","Ę"), ir tada passinti į web API, kol kas dedu Ę nes neturėtų
+             * būti naudojamas komentaruose kaip pirma raidė sakinio. Bet galima sugalvoti kuo kitu keisti.
+             */
+            var regex = new Regex(@"(?<=Ę)\w+");
+            var matches = regex.Matches(comment);
+
+            foreach (Match m in matches)
+            {
+                _hashTags.Add(m.Value);
+
+            }
+            return _hashTags;
+        }
+        /// <summary>
+        /// Patikrina ar paduoti tinkami kintamieji
+        /// Ar rate mažiau lygu 5 ir daugiau nei 0
+        /// Ar beverage mažiau lygu 10 ir daugiau nei 0 
+        /// Ar baro vardas yra bent 1 simblos 
+        public Boolean Validation(int rate, int beverage, string barName)
+        {
+            if (rate > MAX_RATE || beverage > MAX_BEVERAGE_LEVEL || barName.Length < MIN_NAME_LENGHT || rate <= MIN_BEVERAGE_RATE_LEVEL || beverage <= MIN_BEVERAGE_RATE_LEVEL)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
--- Social-tap-API/Social-tap-API/Controllers/ValuesController.cs
+++ Social-tap-API/Social-tap-API/Controllers/ValuesController.cs
@@ -16,15 +16,9 @@ namespace Social_tap_API.Controllers
         public const int MAX_RATE = 5; // kiek daugiausiai gali duoti žvaigždučių 
         public const int MIN_NAME_LENGHT = 1; //trumpiausias įmanomas baro pavadinimas
         public const int MIN_BEVERAGE_RATE_LEVEL = 0; //kiek mažiausiai gali įpilti ir duoti žvaigždučių 
-        private static double _sum;
-        private static int _uses;
-        private List<string> _hashTags = new List<string>();
-        private static Dictionary<string, List<string>> _barInfo;
-        private static Dictionary<string, List<int>> _barRates = new Dictionary<string, List<int>>();
         public static Dictionary<string, BarData> _barData = new Dictionary<string, BarData>();
-        public ValuesController()
-        {
-            _barInfo = new Dictionary<string, List<string>>();
+        public ValuesController(){
+
         }
         /// Metodui paduodami 4 parametrai
         /// Patikrinama ar jie atitinka reikalavimus
@@ -34,14 +28,13 @@ namespace Social_tap_API.Controllers
         [HttpPost("AddBarReview/{barName}/{comment}/{rate}/{beverage}")]
         public Boolean AddBarReview(string barName, string comment, int rate, int beverage) //rate-žvaigždutės, beverage-įpilto alaus lygis
         {
+            Calculations calc = new Calculations();
             Console.WriteLine($"POST: AddBarReview/{barName}/{comment}/{rate}/{beverage}");
-            if (rate > MAX_RATE || beverage > MAX_BEVERAGE_LEVEL || barName.Length < MIN_NAME_LENGHT || rate<= MIN_BEVERAGE_RATE_LEVEL || beverage<= MIN_BEVERAGE_RATE_LEVEL)
+            if (!calc.Validation(rate, beverage, barName))
             {
                 return false;
             }
-            barName = barName.ToLower();
-            barName = barName.Replace(" ", string.Empty).Replace("-", string.Empty).Replace(".", string.Empty);  // pasalinam visus tarpus, taškus ir -
-            barName = char.ToUpper(barName[0]) + barName.Substring(1);
+            barName = calc.BarNameAdaptation(barName);
             if (!_barData.Keys.Contains(barName))
             {
                 _barData.Add(barName,new BarData());
@@ -49,9 +42,9 @@ namespace Social_tap_API.Controllers
             
             _barData[barName].BarUses++;
             _barData[barName].BeverageSum += beverage;
-            _barData[barName].Comparison = Average(beverage);
-            _barData[barName].Tags.AddRange(HashtagsFinder(comment)); 
-            _barData[barName].RateAvg = BarRateAverage(barName, rate);
+            _barData[barName].Comparison = calc.Average(beverage);
+            _barData[barName].Tags.AddRange(calc.HashtagsFinder(comment)); 
+            _barData[barName].RateAvg = calc.BarRateAverage(barName, rate);
             _barData[barName].BeverageAvg = _barData[barName].BeverageSum/ _barData[barName].BarUses;
             return true;
 
@@ -66,82 +59,6 @@ namespace Social_tap_API.Controllers
             return _barData;
 
         }
-
-        /// Į metodą paduodamas baro pavadinimas ir jo įvertinimas
-        /// Web service laikome baro pavadinimą ir jo įvertinimų Listą 
-        /// Atgal grąžiname tik baro įvertinimų vidurkį
-        /// http://localhost:.../api/values/barrate/string_baropavadinimas/string_įvertinimas
-
-        public double BarRateAverage(string barName, int rate)
-        {
-
-            if (_barRates.Keys.Contains(barName))
-            {
-                _barRates[barName].Add(rate);
-            }
-            else
-            {
-                _barRates[barName] = new List<int> { rate };
-            }
-            return _barRates[barName].Average();
-        }
         
-        /// Į metodą paduodama informacija kiek buvo įpilta 
-        /// Web service saugoma informacija apie tai kiek kartų buvo pasinaudota programa 
-        /// Grąžiname true jeigu įpilta geriau, false jeigu blogau. 
-        /// Lyginimas vyksta ne su vieno baro statistika o su BENDRA 
-        /// http://localhost:.../api/values/bevlvl/INT
-       // [HttpPost("bevlvl/{beverageLevel}")]    
-        public Boolean Average(int beverageLevel)
-        {
-            _uses++;
-            _sum += beverageLevel;
-            // sakysime, kad jeigu lygus vidurkiui, 
-            // tai ipilta geriau
-            return _sum / _uses <= beverageLevel;  
-        }
-        /// Į metodą paduodamas komentaras 
-        /// Iš jo išrenkami žodžiai panaudoti su hashtagu 
-        /// Grąžinamas hashtagų listas 
-        /// http://localhost:.../api/values/tags/STRING
-        //  [HttpPost("tags/{comment}")]
-        public List<string> HashtagsFinder(string comment) 
-        {
-            /*
-             * Hashtag'ą programoje reikės pakeist kuo nors kitu naudojant kintamasis.
-             * Replace("#","Ę"), ir tada passinti į web API, kol kas dedu Ę nes neturėtų
-             * būti naudojamas komentaruose kaip pirma raidė sakinio. Bet galima sugalvoti kuo kitu keisti.
-             */
-            var regex = new Regex(@"(?<=Ę)\w+");          
-            var matches = regex.Matches(comment);
-
-            foreach (Match m in matches)
-            {
-                _hashTags.Add(m.Value);
-
-            }
-            return _hashTags;
-        }
-
-        /// Į metodą paduodamas baro pavadinimas ir komentaras 
-        /// Su baro pavadinimu, kuris yra Dictionary raktas pridedami hashtagai. 
-        /// Jie gaunami iškvietus HashtagsFinder metodą
-        /// Grąžinamas dictionary.
-        /// http://localhost:.../api/values/names/STRING_baroPavadinimas/STRING_komentaras
-       // [HttpPost("names/{barName}/{comment}")]
-        public Dictionary<string, List<string>> CountBars(string barName, string comment)
-        {
-            
-            if (_barInfo.Keys.Contains(barName))
-            {
-                _barInfo[barName].AddRange(HashtagsFinder(comment));
-            }
-            else
-            {
-                _barInfo[barName] = HashtagsFinder(comment);
-            }
-            
-            return _barInfo;
-        }
     }
 }
