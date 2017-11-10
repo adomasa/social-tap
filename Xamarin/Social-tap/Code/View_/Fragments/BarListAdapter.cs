ccbb3b92207f9d5d55c9708a9d1d0944c31797b0
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-10 08:50:29 +0200

    Upgrade BarList adapter

--- Xamarin/Social-tap/Code/View_/Fragments/BarListAdapter.cs
+++ Xamarin/Social-tap/Code/View_/Fragments/BarListAdapter.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
@@ -16,12 +18,14 @@ namespace Socialtap.Code.View_.Fragments
             internal readonly TextView Title;
             internal readonly TextView BarRating;
             internal readonly TextView AvgBeverageVolume;
+            internal readonly TextView ReviewCount;
 
             public BarListViewHolder(View v) : base(v)
             {
                 Title = v.FindViewById<TextView>(Resource.Id.barTitle);
                 BarRating = v.FindViewById<TextView>(Resource.Id.barRating);
                 AvgBeverageVolume = v.FindViewById<TextView>(Resource.Id.avgBeverageVolume);
+                ReviewCount = v.FindViewById<TextView>(Resource.Id.reviewCount);
             }
         }
 
@@ -40,14 +44,17 @@ namespace Socialtap.Code.View_.Fragments
         /// Pakeičia view'o turinį užkraunant/scrollinant
         public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
         {
-            var item = MainActivityController.BarsData[position];
+
+            var item = MainController.BarsData.ElementAt(position);
+            
 
             if (!(holder is BarListViewHolder viewHolder)) return;
-            viewHolder.Title.Text = item.BarName;
-            viewHolder.BarRating.Text = item.AverageRating.ToString();
-            viewHolder.AvgBeverageVolume.Text = item.AverageBeverageVolume.ToString();
+            viewHolder.Title.Text = MainController.BarsData.ElementAt(position).Key;
+            viewHolder.BarRating.Text = item.Value.RateAvg.ToString();
+            viewHolder.AvgBeverageVolume.Text = item.Value.BeverageAvg.ToString();
+            viewHolder.ReviewCount.Text = item.Value.BarUses.ToString();
         }
 
-        public override int ItemCount => MainActivityController.BarsData.Count;
+        public override int ItemCount => MainController.BarsData.Count;
     }
 }
