2225d2b0e2ddfe327c90da7946414ef2de5690d9
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-15 22:36:37 +0200

    Add statistics model class and interface

---
+++ Xamarin/Social-tap/Code/Model/Interfaces/IStatistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Socialtap.Code.Model
+{
+    public interface IStatistics
+    {
+        string TopBarName { get; set; }
+        double TopBarRate { get; set; }
+        double TopBarAvgBeverageVolume { get; set; }
+        double TotalAvgBeverageVolume { get; set; }
+        int BarCount { get; set; }
+        int ReviewCount { get; set; }
+    }
+}
