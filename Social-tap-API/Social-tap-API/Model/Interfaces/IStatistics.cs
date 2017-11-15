649741f56cff77f52ca475faab360f70cdd68340
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-15 22:32:24 +0200

    Add statistics model class with interface

---
+++ Social-tap-API/Social-tap-API/Model/Interfaces/IStatistics.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialtapAPI
+{
+    public interface IStatistics
+    {
+        string TopBarName { get; set; }
+        double TopBarRate { get; set; }
+        double TopBarAvgBeverageVolume { get; set; }
+        double TotalAvgBeverageVolume { get; set; }
+        int BarCount { get; set; }
+        int ReviewCount { get; set; }
+    }
+}
