443b2e01a9eec378698c945aeef7adc9fd60fb4d
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-15 22:36:22 +0200

    Add initial home fragment class

---
+++ Xamarin/Social-tap/Code/View_/Fragments/HomeFragment.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+
+namespace Socialtap.Code.View_.Fragments
+{
+    public class HomeFragment : Fragment
+    {
+        private View _rootView;
+
+        public static HomeFragment NewInstance()
+        {
+            return new HomeFragment();
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            _rootView = inflater.Inflate(Resource.Layout.fragment_home, container, false);
+
+
+            return _rootView;
+        }
+    }
+}
