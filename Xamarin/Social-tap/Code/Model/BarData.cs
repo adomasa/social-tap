1c1247848bff19a4c6f41ef74110d9eb0b63134c
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-15 22:36:49 +0200

    Uprade bar data model class

--- Xamarin/Social-tap/Code/Model/BarData.cs
+++ Xamarin/Social-tap/Code/Model/BarData.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections.Generic;
-using Socialtap.Code.Model.SocialtapAPI;
 
 namespace Socialtap.Code.Model
 {
     public class BarData : IBarData
     {
-        public List<string> Tags { get; set; } //saugomi hashtagai
-        public double RateAvg { get; set; }  //žvaigždučių vidurkis
+        //saugomi hashtagai
+        public List<string> Tags { get; }
+        //žvaigždučių vidurkis
+        public double RateAvg { get; set; }
+        //ar geriau įpylė visų barų palyginime
+        public bool Comparison { get; set; }
+        //baro vidurkis
+        public double BeverageAvg { get; set; }
+        //kiek kartų buvo pasinaudota programele konkrečiame bare 
+        public int BarUses { get; set; }
+        //kiek iš viso buvo įpilta bare
+        public int BeverageSum { get; set; }
 
-        public bool Comparison { get; set; }   //ar geriau įpylė visų barų palyginime
-
-        public double BeverageAvg { get; set; } //baro vidurkis
-
-        public  int BarUses { get; set; } //kiek kartų buvo pasinaudota programele konkrečiame bare 
-        public int BeverageSum { get; set; } //kiek iš viso buvo įpilta bare
-
-        public BarData() {
+        public BarData()
+        {
             Tags = new List<string>();
-            //HashTags = new Dictionary<string, List<string>>();
         }
     }
 }
