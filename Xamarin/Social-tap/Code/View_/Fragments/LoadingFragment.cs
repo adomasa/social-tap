79997cc5d1f26e5c0ac5772f5e0914436facd6f8
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-15 22:35:02 +0200

    Upgrade loading fragment

--- Xamarin/Social-tap/Code/View_/Fragments/LoadingFragment.cs
+++ Xamarin/Social-tap/Code/View_/Fragments/LoadingFragment.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Socialtap.Code.Controller;
@@ -8,32 +9,54 @@ namespace Socialtap.Code.View_.Fragments
 {
     public class LoadingFragment : Fragment
     {
+
+        private const string _targetFragment = "TARGET_FRAGMENT";
         private View _rootView;
         private Fragment _fragment;
-        private bool _status;
+        private bool request_success;
         private TextView _errorLabel;
         private ProgressBar _progressBar;
 
-        public static LoadingFragment NewInstance()
+        public static LoadingFragment NewInstance(int fragment_id)
         {
-            return new LoadingFragment();
+            LoadingFragment fragment = new LoadingFragment();
+            Bundle args = new Bundle();
+            args.PutInt(_targetFragment, fragment_id);
+            fragment.Arguments = args;
+            return fragment;
         }
 
         public override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            _fragment = BarListFragment.NewInstance();
-            // Laukia kol parsiųs duomenis
-            _status = await MainController.FetchBarsData();
+            int fragmentId = Arguments.GetInt(_targetFragment);
+               
+            switch (fragmentId) {
+                case Resource.Id.fragment_home:
+                    _fragment = HomeFragment.NewInstance();
+                    request_success = await MainController.FetchStatistics();
 
-            if (_status)
+                    break;
+                case Resource.Id.fragment_bar_list:
+                    _fragment = BarListFragment.NewInstance();
+                    request_success = await MainController.FetchBarsData();
+                    break;
+                default:
+                    Log.Error(Tag, $"LoadingFragment onCreate(): " +
+                              "Unknown fragmentId");
+                    DisplayError();
+                    return;
+            }
+
+            if (request_success)
             {
-                LoadBarListFragment();
+                FragmentManager.BeginTransaction()
+                               .Replace(Resource.Id.content_frame, _fragment)
+                               .Commit();
             }
             else
             {
-                _progressBar.Visibility = ViewStates.Gone;
-                _errorLabel.Visibility = ViewStates.Visible;
+                DisplayError();
             }
         }
 
@@ -46,8 +69,9 @@ namespace Socialtap.Code.View_.Fragments
             return _rootView;
         }
 
-        void LoadBarListFragment() => FragmentManager.BeginTransaction()
-            .Replace(Resource.Id.content_frame, _fragment)
-            .Commit();
+        void DisplayError() {
+            _progressBar.Visibility = ViewStates.Gone;
+            _errorLabel.Visibility = ViewStates.Visible;
+        }
     }
 }
