1acbf9ff37f2cc23d2943520678ffa3b1ab533a5
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-24 01:05:32 +0200

    Transfer maincontroller requests into wrapper/bridge

--- Xamarin/Social-tap/Code/Controller/MainController.cs
+++ Xamarin/Social-tap/Code/Controller/MainController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Android.Content;
 using Android.Util;
 using Newtonsoft.Json;
 using RestSharp;
@@ -9,60 +10,66 @@ using Socialtap.Code.Model;
 
 namespace Socialtap.Code.Controller
 {
-    public static class MainController
+    public class MainController
     {
         private const string Tag = "MainController";
         private const string Url = "http://10.0.2.2:5000";
+
         public static Dictionary<string, BarData> BarsData;
-        public static Statistics stats;
-        public static CancellationTokenSource cancellationTokenSource;
+        public static IStatistics stats;
 
-        private const int PostRequestTimeout = 3000;
-        private const int GetRequestTimeout = 5000;
-        private const bool status_failed = false;
+        private RequestManager requestManager;
+        Context context;
+        MainActivity activity;
 
+        private static MainController instance;
+
+        private MainController(Context context)
+        {
+            this.context = context;
+            requestManager = RequestManager.GetInstance(context);
+            activity = (MainActivity) context;
+        }
+
+        public static MainController GetInstance(Context context)
+        {
+            instance = instance ?? new MainController(context);
+            return instance;
+        }
+
+        public void AbortRequest() 
+        {
+            requestManager.AbortRequest();
+        }
+
+        //Todo: DI, lazy,
+        //Todo: Kai paleistas async taskas, negali kitas būt leidžiamas
 
         /// <summary>
         /// Adds the bar review via http request to web API
         /// </summary>
-        /// <param name="barReview">Bar review, collection of target data</param>
-        /// <param name="mainActivity">Reference to access view methods</param>
-        public static async void AddBarReview(BarReview barReview, MainActivity mainActivity)
+        /// <param name="barReview">Parsed user entry</param>
+        public async void AddBarReview(IBarReview barReview)
         {
             Log.Debug(Tag, "AddBarReview called.");
-            var client = new RestClient(Url);
-            var request = new RestRequest($"/api/values/AddBarReview/{barReview.BarName}/{barReview.Comment}/" +
-                                          $"{barReview.Rating}/{barReview.BeverageVolume}", Method.POST)
-            {
-                Timeout = PostRequestTimeout
-            };
+            string content = await requestManager.PostBarReview(barReview);
 
-            cancellationTokenSource = new CancellationTokenSource();
-            // Execution
-            var response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
-
-            if (cancellationTokenSource != null)
+            if (content == null || content.Equals(String.Empty))
             {
-                cancellationTokenSource.Dispose();
+                activity.ReportAddBarReviewState(false);  
             }
-            // Result handling
-            var requestStatus = response.ResponseStatus;
-            // Failed request
-            if (requestStatus.Equals(ResponseStatus.None) || requestStatus.Equals(ResponseStatus.TimedOut) 
-                || requestStatus.Equals(ResponseStatus.Error))
-                mainActivity.ReportAddBarReviewState(status_failed);
             else
             {
-                // Successful request
                 Log.Debug(Tag, "AddBarReview deserialization in progress.");
-                try 
+                try
                 {
-                    var processingStatus = JsonConvert.DeserializeObject<bool>(response.Content);
-                    mainActivity.ReportAddBarReviewState(requestStatus.Equals(ResponseStatus.Completed) && processingStatus);
+                    var processingStatus = JsonConvert.DeserializeObject<bool>(content);
+                    activity.ReportAddBarReviewState(processingStatus);
                 }
-                catch (Exception e) {
+                catch (Exception e)
+                {
                     Log.Error(Tag, $"Exception while handling json response. Message: {e.Message}");
-                    mainActivity.ReportAddBarReviewState(requestStatus.Equals(ResponseStatus.Completed) && status_failed);
+                    activity.ReportAddBarReviewState(false);
                 }
             }
         }
@@ -71,74 +78,38 @@ namespace Socialtap.Code.Controller
         /// Fetchs bars data from web API
         /// </summary>
         /// <returns>Collection of BarData</returns>
-        public static async Task<bool> FetchBarsData()
+        public async Task<bool> FetchBarsData()
         {
-            //Todo: halt async request when leaving unloaded fragment
             Log.Debug(Tag, "FetchBarsData called.");
-            var client = new RestClient(Url);
-            var request = new RestRequest("/api/values/GetBarData/", Method.GET) {Timeout = GetRequestTimeout};
+            string content = await requestManager.GetBarsData();
 
-            cancellationTokenSource = new CancellationTokenSource();
-            // Execution
-            var response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
-
-            if (cancellationTokenSource != null)
+            var status = content != null;
+            if (status)
             {
-                cancellationTokenSource.Dispose();
+                Log.Debug(Tag, "FetchBarsData deserialization in progress.");
+                BarsData = JsonConvert.DeserializeObject<Dictionary<string, BarData>>(content);
             }
-            // Result handling
-            var requestStatus = response.ResponseStatus;
-
-            // Failed request
-            if (requestStatus.Equals(ResponseStatus.None) || requestStatus.Equals(ResponseStatus.TimedOut)
-                || requestStatus.Equals(ResponseStatus.Error))
-            {
-                Log.Debug(Tag, "FetchBarsData request failed.");
-                return false;
-            }
-            // Successful request
-            var content = response.Content;
-            Log.Debug(Tag, "FetchBarsData deserialization in progress.");
-            BarsData = JsonConvert.DeserializeObject<Dictionary<string, BarData>>(content);
-            return requestStatus.Equals(ResponseStatus.Completed);
+            return status;
         }
+
+
         /// <summary>
         /// Fetches statistics from web API
         /// </summary>
         /// <returns>Statistics object</returns>
-        public static async Task<bool> FetchStatistics()
+        public async Task<bool> FetchStatistics()
         {
-            //Todo: halt async request when leaving unloaded fragment
             Log.Debug(Tag, "FetchStatistics called.");
 
-            BarsData = new Dictionary<string, BarData>();
-            var client = new RestClient(Url);
-            var request = new RestRequest("/api/values/Stats/", Method.GET) { Timeout = GetRequestTimeout };
+            string content = await requestManager.GetStatData();
 
-            cancellationTokenSource = new CancellationTokenSource();
-            // Execution
-            var response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
-
-            if (cancellationTokenSource != null)
+            var status = content != null;
+            if (status)
             {
-                cancellationTokenSource.Dispose();
+                Log.Debug(Tag, "FetchStatistics deserialization in progress.");
+                stats = JsonConvert.DeserializeObject<Statistics>(content);
             }
-            // Result handling
-            var requestStatus = response.ResponseStatus;
-
-            // Failed request
-            if (requestStatus.Equals(ResponseStatus.None) || requestStatus.Equals(ResponseStatus.TimedOut)
-                || requestStatus.Equals(ResponseStatus.Error))
-            {
-                return status_failed;
-            }
-
-            //Succesful request
-            var content = response.Content;
-            Log.Debug(Tag, "FetchStatistics deserialization in progress.");
-            stats = JsonConvert.DeserializeObject<Statistics>(content);
-
-            return requestStatus.Equals(ResponseStatus.Completed);
+            return status;
         }
     }
 }
---
+++ Xamarin/Social-tap/Code/Controller/RequestManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Util;
+using RestSharp;
+using Socialtap.Code.Model;
+
+namespace Socialtap.Code.Controller
+{
+    public class RequestManager
+    {
+        public static string Tag = "RequestManager";
+
+        private Context context;
+        private string apiUrl;
+        private int getRequestTimeout;
+        private int postRequestTimeout;
+        private static RequestManager instance;
+
+        public static CancellationTokenSource cancellationTokenSource;
+
+        private RequestManager(Context context) 
+        {
+            this.context = context;
+            int.TryParse(context.GetString(Resource.String.get_request_timeout), out getRequestTimeout);
+            int.TryParse(context.GetString(Resource.String.post_request_timeout), out postRequestTimeout);
+            apiUrl = context.GetString(Resource.String.api_url);
+        }
+
+        public static RequestManager GetInstance (Context context)
+        {
+            instance = instance ?? new RequestManager(context);
+            return instance;
+        }
+
+        public async Task<string> PostBarReview(IBarReview review) 
+        {
+            var requestPathFormat = context.GetString(Resource.String.post_bar_review_path);
+            var requestPath = String.Format(requestPathFormat, review.BarName, review.Comment, review.Rating, review.BeverageVolume);
+            // returns request result raw content
+            var sth = await BaseRequest(requestPath, Method.POST);
+            return sth;
+        }
+
+        public async Task<string> GetBarsData()
+        {
+            var requestPath = context.GetString(Resource.String.get_bar_data_path);
+
+            // returns request result raw content
+            return await BaseRequest(requestPath, Method.GET);
+        }
+
+        public async Task<string> GetStatData()
+        {
+            var requestPath = context.GetString(Resource.String.get_stats_path);
+
+            // returns request result raw content
+            return await BaseRequest(requestPath, Method.GET);
+        }
+
+        private async Task<string> BaseRequest(string requestPath, Method method) 
+        {
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest(requestPath, method)
+            {
+                Timeout = postRequestTimeout
+            };
+
+            IRestResponse response;
+
+            cancellationTokenSource = new CancellationTokenSource();
+            Log.Debug(Tag, "CancellationTokenSource initialised");
+
+            try
+            {
+                response = await client.ExecuteTaskAsync(request, cancellationTokenSource.Token);
+                if (cancellationTokenSource != null)
+                {
+                    cancellationTokenSource.Dispose();
+                    cancellationTokenSource = null;
+                    Log.Debug(Tag, "CancellationTokenSource disposed");
+                }
+                    
+                    var requestStatus = response.ResponseStatus;
+                    return requestStatus.Equals(ResponseStatus.Completed) ? response.Content : null;
+                
+            }
+            catch (TaskCanceledException e) 
+            {
+                Log.Debug(Tag, "Task cancelled");
+                return null;
+            }
+        }
+
+        public void AbortRequest()
+        {
+            if (cancellationTokenSource != null)
+            {
+                cancellationTokenSource.Cancel();
+                Log.Debug(Tag, "CancellationToken activated");
+            }
+            else 
+            {
+                Log.Debug(Tag, "There is no CancellationToken");    
+            }
+        }
+    }
+}
