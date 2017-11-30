8626644e2a9f097bbbf1dbfab86e22851ef3d000
Adomas <31891228+ramumnesys@users.noreply.github.com>
2017-11-30 20:27:42 +0200

    Implement cfg file handler into controller

--- Xamarin/Social-tap/Code/Controller/MainController.cs
+++ Xamarin/Social-tap/Code/Controller/MainController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Android.Content;
 using Android.Util;
 using Newtonsoft.Json;
-using RestSharp;
 using Socialtap.Code.Controller.Interfaces;
 using Socialtap.Code.Model;
 
@@ -16,35 +14,38 @@ namespace Socialtap.Code.Controller
         private const string Tag = "MainController";
         private const string Url = "http://10.0.2.2:5000";
 
-        public static Dictionary<string, BarData> BarsData;
+        public static Dictionary<string, IBarData> BarsData;
         public static IStatistics stats;
 
-        private IRequestManager requestManager;
-        Context context;
-        MainActivity activity;
+        private IRequestManager _requestManager;
+        private Context _context;
+        private MainActivity _activity;
 
         private static MainController instance;
 
         private MainController(Context context)
         {
-            this.context = context;
-            requestManager = RequestManager.GetInstance(context);
-            activity = (MainActivity) context;
+            _context = context;
+            _requestManager = RequestManager.GetInstance(context);
+            _activity = (MainActivity) context;
+
+            //load cfg settings
+            var propertiesHandler = PropertiesHandler.GetInstance(context);
+
+            var url = propertiesHandler.GetConfigValue("api_url");
         }
 
         public static MainController GetInstance(Context context)
         {
-            instance = instance ?? new MainController(context);
-            return instance;
+            return instance ?? new MainController(context);
         }
 
         public void AbortRequest() 
         {
-            requestManager.AbortRequest();
+            _requestManager.AbortRequest();
         }
 
         //Todo: DI, lazy,
-        //Todo: Kai paleistas async taskas, negali kitas būt leidžiamas
 
         /// <summary>
         /// Adds the bar review via http request to web API
@@ -53,11 +54,11 @@ namespace Socialtap.Code.Controller
         public async void AddBarReview(IBarReview barReview)
         {
             Log.Debug(Tag, "AddBarReview called.");
-            string content = await requestManager.PostBarReview(barReview);
+            string content = await _requestManager.PostBarReview(barReview);
 
             if (content == null || content.Equals(String.Empty))
             {
-                activity.ReportAddBarReviewState(false);  
+                _activity.ReportAddBarReviewState(false);  
             }
             else
             {
@@ -65,12 +66,12 @@ namespace Socialtap.Code.Controller
                 try
                 {
                     var processingStatus = JsonConvert.DeserializeObject<bool>(content);
-                    activity.ReportAddBarReviewState(processingStatus);
+                    _activity.ReportAddBarReviewState(processingStatus);
                 }
                 catch (Exception e)
                 {
                     Log.Error(Tag, $"Exception while handling json response. Message: {e.Message}");
-                    activity.ReportAddBarReviewState(false);
+                    _activity.ReportAddBarReviewState(false);
                 }
             }
         }
@@ -82,13 +83,13 @@ namespace Socialtap.Code.Controller
         public async Task<bool> FetchBarsData()
         {
             Log.Debug(Tag, "FetchBarsData called.");
-            string content = await requestManager.GetBarsData();
+            string content = await _requestManager.GetBarsData();
 
             var status = content != null;
             if (status)
             {
                 Log.Debug(Tag, "FetchBarsData deserialization in progress.");
-                BarsData = JsonConvert.DeserializeObject<Dictionary<string, BarData>>(content);
+                BarsData = JsonConvert.DeserializeObject<Dictionary<string, IBarData>>(content);
             }
             return status;
         }
@@ -102,7 +103,7 @@ namespace Socialtap.Code.Controller
         {
             Log.Debug(Tag, "FetchStatistics called.");
 
-            string content = await requestManager.GetStatData();
+            string content = await _requestManager.GetStatData();
 
             var status = content != null;
             if (status)
